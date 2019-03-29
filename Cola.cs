fa48f4d2f4b88af54e929fefcb9a2d8a36273d9b
Hector Marcial <48300532+hmarcial@users.noreply.github.com>
2019-03-28 18:47:49 -0600

    Clase Coladera y Form

---
+++ Cola.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea
+{
+    class Cola
+    {
+        private int [] vec;
+        public Cola()
+        {
+            vec = new int [1000];
+        }
+        public void llenar()
+        {
+            for(int i = 1; i < 1000; i++)
+            {
+                vec[i] = 1;
+            }
+        }
+        public void Compara()
+        {
+            for(int i =2; i < 1000; i++)
+            {
+                if (vec[i] == 1)
+                {
+                    for(int h = i+1; h < 1000; h++)
+                    {
+                        if (h % i == 0)
+                        {
+                            vec[h] = 0;
+                        }
+                    }
+                }
+            }
+        }
+        public string ver()
+        {
+            string resultado = "";
+            for(int i = 2; i < vec.Length - 1; i++)
+            {
+                if(vec[i] == 1)
+                {
+                    resultado += i + " Primo " + Environment.NewLine;
+                }
+            }
+            return resultado;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea
+{
+    public partial class Form1 : Form
+    {
+        Cola v = new Cola();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btndale_Click(object sender, EventArgs e)
+        {
+            v.llenar();
+            v.Compara();
+            txtr.Text = v.ver();
+        }
+    }
+}
